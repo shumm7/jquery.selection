9d8039a3934d479fc39b99560474dfb3b48b4bca
しゅう <44467870+shumm7@users.noreply.github.com>
2024-10-20 23:44:38 +0900

    dist

--- src/jquery.selection.ts
+++ src/jquery.selection.ts
@@ -52,13 +52,13 @@ interface JQuerySelection_Opts {
 }
 
 interface JQuerySelection_ReplaceOpts extends JQuerySelection_Opts {
-    text: JQuerySelection_Text
+    text?: JQuerySelection_Text
     caret?: JQuerySelection_Caret
 }
 
 interface JQuerySelection_InsertOpts extends JQuerySelection_Opts {
-    text: JQuerySelection_Text
-    mode: JQuerySelection_InsertMode
+    text?: JQuerySelection_Text
+    mode?: JQuerySelection_InsertMode
     caret?: JQuerySelection_Caret
 }
 
@@ -68,20 +68,22 @@ interface JQuerySelection_Range extends JQuerySelection_Opts {
 }
 
 export interface JQuerySelection extends JQuery {
-    /**
+    /** 
+     * ## .selection('replace', opts)
      * 選択文字列を置き換えます
-     * @param {string} opts.text - 置き換え後の文字列
+     * @param {JQuerySelection_Text} opts.text - 置き換え後の文字列
      * 
      * @param {JQuerySelection_Caret} opts.caret - "keep", "start", "end" のいずれか。 指定がない場合、"keep" として処理されます。
      * * "keep" - 選択状態を保持させる（置き換え後の文字列が選択される）
      * * "start" - 選択開始位置にキャレットを移動させる
      * * "end" - 選択終了位置にキャレットを移動させる
      */
-    selection(mode: "replace", opts: JQuerySelection_ReplaceOpts): JQuery<HTMLElement>;
+    selection(mode: "replace", opts?: {text?: string, caret?: "keep"|"start"|"end"}): JQuery<HTMLElement>;
 
     /**
+     * ## .selection('insert', opts)
      * 選択文字列の前、もしくは後に文字列を挿入します
-     * @param {string} opts.text - 挿入する文字列
+     * @param {JQuerySelection_Text} opts.text - 挿入する文字列
      * 
      * @param {JQuerySelection_InsertMode} opts.mode - 挿入モード "before", "after" のいずれか。
      * * "before" - 選択開始位置（選択文字列の前）に文字列を挿入する
@@ -92,38 +94,42 @@ export interface JQuerySelection extends JQuery {
      * * "start" - 選択開始位置にキャレットを移動させる
      * * "end" - 選択終了位置にキャレットを移動させる
      */
-    selection(mode: "insert", opts: JQuerySelection_InsertOpts): JQuery<HTMLElement>;
+    selection(mode: "insert", opts?: {text?: string, mode?: "before"|"after", caret?: "keep"|"start"|"end"}): JQuery<HTMLElement>;
 
     /**
+     * ## .selection('get')
      * 選択されている文字列を取得します
-     * @returns {string} - 選択されている文字列を返します
+     * @returns {JQuerySelection_Text} - 選択されている文字列を返します
      */
     selection(mode: "get"): string;
 
     /**
+     * ## .selection('getPos')
      * キャレット位置を取得します
      * @returns {JQuerySelection_Range} - キャレット位置を返します
      * * return.start {Integer} - 選択開始位置
      * * return.end {Integer} - 選択終了位置
      */
-    selection(mode: "getPos"): JQuerySelection_Range;
+    selection(mode: "getPos"): {start: number, end: number};
 
     /**
+     * ## .selection('setPos', opts)
      * キャレット位置を設定します
-     * @param {number} opts.start - 選択開始位置（位置は0から数えます）
-     * @param {number} opts.end - 選択終了位置（位置は0から数えます）
+     * @param {JQuerySelection_Number} opts.start - 選択開始位置（位置は0から数えます）
+     * @param {JQuerySelection_Number} opts.end - 選択終了位置（位置は0から数えます）
      */
-    selection(mode: "setPos", opts: JQuerySelection_Range): JQuery<HTMLElement>;
+    selection(mode: "setPos", opts?: {start?: number, end?: number}): JQuery<HTMLElement>;
 
     /**
+     * ## jQuery.selection(mode)
      * ウィンドウ内の選択されている文字列を取得します
      * @param {JQuerySelection_SelectionMode} mode - 取得モード "text", "html" のいずれか。 指定がない場合、"text" として処理されます。
      * * "text" - テキストを取得します
      * * "html" - HTMLを取得します
      * 
-     * @returns {string} - 選択されている文字列を返します。
+     * @returns {JQuerySelection_Text} - 選択されている文字列を返します。
      */
-    selection(mode?: JQuerySelection_SelectionMode): string;
+    selection(mode?: "text"|"html"): string;
 }
 
 (function($: JQueryStatic, win: Window, doc: Document) {
@@ -198,7 +204,7 @@ export interface JQuerySelection extends JQuery {
             element.focus();
             try {
                 if (element.setSelectionRange) {
-                    element.setSelectionRange(toRange.start ?? 0, toRange.end ?? 0);
+                    element.setSelectionRange(toRange.start ?? null, toRange.end ?? null);
                 }
             } catch (e) {
                 /* give up */
